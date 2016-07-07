d2e987c7953a35b5dfe5746c1dd61187e21cf8bc
goodmove <goodmoveab@gmail.com>
2016-07-07 17:00:40 +0600

    Bugs fixes

--- bot_io/COMMANDS.py
+++ bot_io/COMMANDS.py
@@ -1,9 +1,9 @@
 import requests
 
 def send_message(url, token, chat_id, text):
-    method_name = "/sendMessage"
-    response = requests.get(url + token + method_name, {"chat_id" : chat_id, "text" : text})
+    url = "{0}{1}{2}/sendMessage".format(url, token, method_name)
+    payload = {"chat_id" : chat_id, "text" : text}
+    response = requests.get(url , params=payload)
     if int(response.status_code) == 200:
-        # print("success")
         return True
     return False
