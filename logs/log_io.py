7bac7576b86036649b55b26336aa15f6531cb943
good-move <gmalex14@gmail.com>
2016-07-08 11:40:50 +0600

    Fixed issue with cyrillic characters encoding

--- logs/log_io.py
+++ logs/log_io.py
@@ -1,8 +1,9 @@
 import time
+import codecs 
 
 def log_requests(log_file, type, user, chat, msg):
     log_file.write("*"*15 + "\n")
     log_file.write(time.strftime("Timestamp: %d-%m-%Y %H:%M:%S\n"))
     log_file.write("Type: {0}\n".format(type))
     log_file.write(str(user) + "\n")
-    log_file.write("Message: {0}\n\n".format(msg.text))
+    log_file.write(u"Message: {0}\n\n".format(msg.text.encode("utf_8")))
