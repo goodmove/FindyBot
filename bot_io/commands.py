0837e1fafacc0d2504827c96361ac8f6cfccbf0b
RoyPy <bogush.vano@gmail.com>
2016-07-13 13:56:47 +0600

    initial commit

--- bot_io/commands.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def send_message(url, token, chat_id, text):
-    url = "{0}{1}/sendMessage".format(url, token)
-    payload = {"chat_id" : chat_id, "text" : text}
-    response = requests.get(url , params=payload)
-    if int(response.status_code) == 200:
-        return True
-    return False
