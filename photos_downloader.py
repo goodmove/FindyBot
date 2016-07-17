c35b74959a251bc7fef67404862ed0005284d165
RoyPy <bogush.vano@gmail.com>
2016-07-18 00:34:16 +0600

    changed photos_downloader

--- photos_downloader.py
+++ photos_downloader.py
@@ -5,12 +5,11 @@ import time
 import threading as thr
 
 class PhotoDownloader(object):
-	def __init__(self, account_file = 'account', ids_file = None):
+	def __init__(self, account_file = 'account', ids_file = 'ids'):
 		self.account_data 	= None
 		self.ids 		 	= []
 
-		if not ids_file is None:
-			self.updateIds(ids_file)
+		self.updateIds(ids_file)
 
 		self.updateAccountData(account_file)
 
@@ -20,67 +19,112 @@ class PhotoDownloader(object):
 								perms = ['friends', 'photos'])
 
 	def updateIds(self, file_name):
+	"""
+		gets a list of ids from the given file
+		@args
+			file_name – (str). Name of file to get ids from
+	"""
 		f = open(file_name, 'r')
 		self.ids = f.read().strip('[]').split(', ')
 		f.close()
 
 	def updateAccountData(self, file_name):
+	"""
+		gets an account data from the given file and forms it as a dictionary
+		@args
+			file_name – (str). Name of file to get account data from
+	"""
 		f = open(file_name, 'r')
 		data = f.read()
 		f.close()
 		# parse account data to dictionary
 		self.account_data = dict([field.split(':') for field in data.split(',')])
 
-	def downloadAll(self, batch_size = 5, thread_count = 10,
-					path = 'photos/', file_format = '.jpg', field='photo_50'):
-		id_batch = []
+	def downloadAll(self, thread_count = 10, photo_count = 10,
+					path = 'photos', file_format = '.jpg', photo_type = 'm',
+					no_service_albums = 1, need_hidden = 0, skip_hidden = 0):
+	"""
+		downloads photo_count photos for each id in self.ids and stores them in path directory.
+		Photos for each id will be in separate package with name id
+		Note: it skips if a directory with same name already exists (that means this program already worked here)
+
+		@args
+			thread_count – (uint). Max number of threads to use
+			photo_count – (uint). Max number of photos of each user to download
+			path – (str). Path to directory for photos to store
+			file_format – (str). Format of image file
+			photo_type – (str). One of ['s','m','x','y','z','w','o','p','q'] (see description at the end of file)
+	"""
+
+		# make dir if it doesn't exist
+		if not os.path.exists(path):
+			os.makedirs(path)
+
+		if self.ids is None:
+			raise Exception('self.ids is None')
 		for id in self.ids:
-			photo_path = path + id
+			path = path + '/' + id
 			# if file with this name already exists skip it
-			if not os.path.exists(photo_path):
-				os.makedirs(photo_path)
+			if not os.path.exists(path):
+				os.makedirs(path)
 			else: continue
-			# update batch of ids
-			if len(id_batch) < batch_size:
-				id_batch.append(id)
-				continue
 			# prepare payload for request
 			payload = {
-				'user_ids':str([int(i) for i in id_batch]).strip('[]'),
-				'fields':field
+				'owner_id':				id, 
+				'extended':				0,
+				'offset':				0,
+				'count':				photo_count,
+				'photo_sizes':			1,
+				'no_service_albums':	no_service_albums,
+				'need_hidden':			need_hidden,
+				'skip_hidden':			skip_hidden
 			}
-			# don't need ids in id_batch anymore
-			id_batch.clear()
 			# send GET request
-			response = self.api.getRequest('users.get', payload)
+			request = self.api.getRequest('photos.getAll', payload)
 			# skip request error
-			if response is None: continue
-			response = response.get('response')
-			self._fast_download(response, path, file_format, thread_count, field)
+			if 'error' in request: continue
+			response = request.get('response')
+			# create and start threads for downloading photos
+			for index, item in enumerate(response['items']):
+				size = str(item['width']) + 'x' + str(item['height'])
+				photo_name = path + '/' + str(index) + photo_type + size + file_format
+				if os.path.isfile(photo_name): continue
 
-		# if some ids left download them
-		if id_batch:
-			self.downloadAll(self.api, id_batch, batch_size=len(id_batch), field=field)
+				# wait for available threads
+				while thr.active_count() >= thread_count:
+					time.sleep(0.01) # sleep for 10 millis
 
-	def _fast_download(self, response, path, file_format, thread_count, field):
-		for r in response:
-			photo_name = path + str(r['uid']) + '/' + r['first_name'] + '_' + r['last_name'] + file_format
-			if os.path.isfile(photo_name): return
-
-			while thr.active_count() >= thread_count:
-				time.sleep(0.01) # sleep for 10 millis
-
-			new_thread = thr.Thread(target = download, args = (r[field], photo_name))
-			try: new_thread.start()
-			except: print('Runtime Error')
-			print('\rthread count: {0}'.format(thr.active_count()), end='')
-
-
+				new_thread = thr.Thread(target = download, args = (item['src'], photo_name))
+				try: new_thread.start()
+				except: print('couldn\'t start a new thread')
+				# print('\rthread count: {0}'.format(thr.active_count()), end='')
 
 def download(link, name):
+"""
+	downloads a file at link into name
+	@args
+		link – (str). Link to a file that needs to be downloaded
+		name – (str). Name to give to the downloaded file
+	@return
+		True if download was ok
+		False if not
+"""
 	try:
 		url.urlretrieve(link, name)
 		return True
 	except:
 		return False
+
+"""
+	Available values of field 'photo_type'
+		s — proportional copy with 75px max width;
+		m — proportional copy with 130px max width;
+		x — proportional copy with 604px max width;
+		o — if original image's "width/height" ratio is less or equal to 3:2, then proportional copy with 130px max width. If original image's "width/height" ratio is more than 3:2, then copy of cropped by left side image with 130px max width and 3:2 sides ratio.
+		p — еif original image's "width/height" ratio is less or equal to 3:2, then proportional copy with 200px max width. If original image's "width/height" ratio is more than 3:2, then copy of cropped by left side image with 200px max width and 3:2 sides ratio.
+		q — if original image's "width/height" ratio is less or equal to 3:2, then proportional copy with 320px max width. If original image's "width/height" ratio is more than 3:2, then copy of cropped by left side image with 320px max width and 3:2 sides ratio.
+		y — proportional copy with 807px max width;
+		z — proportional copy with 1280x1024px max size;
+		w — proportional copy with 2560x2048px max size.
+"""
 
