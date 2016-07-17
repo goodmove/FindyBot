29b79d91c93d14078508bb92dcdd7ecc8037556b
RoyPy <bogush.vano@gmail.com>
2016-07-18 00:37:52 +0600

    small changes

--- download_photos.py
+++ download_photos.py
@@ -1,4 +1,4 @@
 import photos_downloader as pd
 
-p = pd.PhotoDownloader(account_file='account')
-p.downloadAll(batch_size = 50, thread_count = 50, field='photo_200_orig')
+p = pd.PhotoDownloader()
+p.downloadAll(photo_count=10, thread_count=20)
