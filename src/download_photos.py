aaafddb9966917fd50780877626d4eea2d78b289
RoyPy <bogush.vano@gmail.com>
2016-07-27 14:34:24 +0600

    changed the downloadAll call arguments

--- src/download_photos.py
+++ src/download_photos.py
@@ -1,6 +1,22 @@
 from src.vkapi import photo_downloader as pd
 
 p = pd.PhotoDownloader()
-# p.api.findFriends(p.user_id, depth=2, file_name='ids')
+# p.api.findFriends(p.user_id, depth=1, file_name='ids')
 p.updateIds()
-p.downloadAll(photo_count=80, thread_count=10, show_thread_count=True, no_service_albums=0, check_face=True, photo_type='x', crop=True)
+p.downloadAll(
+	photo_count=80, 
+	thread_count=10, 
+	show_thread_count=True, 
+	check_face=True,
+	path='photos', 
+	face_landmarks='landmarks.txt', 
+	file_format='.jpg', 
+	photo_type='x', 
+	no_service_albums=0, 
+	create_id_folders=False, 
+	keep_old=False, 
+	displacement=None, 
+	extend=False, 
+	crop=False, 
+	resize=False
+)
